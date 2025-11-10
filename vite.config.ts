2d75fe4ea0942ba73ed8c188bfc74a09272bfde4
fugazrococo-boop <fugazrococo@gmail.com>
2025-11-09 22:36:06 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // IMPORTANT: Replace '<repository-name>' with the name of your GitHub repository.
-  // For example, if your repository URL is https://github.com/user/my-project,
+  // For example, if your repository URL is https://github.com/fugazrococo-boop/my-project,
   // set base to '/my-project/'.
-  base: '/seguimiento-proyectos-est150/', 
+  base: '/150proyectos/', 
   plugins: [react()],
 })
